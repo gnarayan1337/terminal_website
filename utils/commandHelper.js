54672220278caf9a70514889dd425e389deddbce
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-02-10 01:53:42 -0600

    Update commandHelper.js

--- utils/commandHelper.js
+++ utils/commandHelper.js
@@ -97,7 +97,7 @@ export const CONTENTS = {
   blog: () => {
     
     
-    window.open("gautam_narayan_cv.pdf", "_blank");
+    window.open(gautam_narayan_cv.pdf, "_blank");
     return "";
   },
 };
