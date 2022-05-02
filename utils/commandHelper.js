0e63fc3e00ba7bf9e2d2d8015eaace7635c6f806
Gautam Narayan <98248099+gnarayan1337@users.noreply.github.com>
2022-05-01 19:29:46 -0500

    Update commandHelper.js

--- utils/commandHelper.js
+++ utils/commandHelper.js
@@ -80,7 +80,7 @@ export const CONTENTS = {
   education:
     () => `I am a Grade 11 student at <a href="https://www.allenisd.org/allenhs" target="_blank">Allen High School</a>
     <br />
-   , the Tech Club.`,
+ ,
   skills: () => `
   I am experienced with Javascript, Typescript and Python and the web technologies dominating at the time:<br />
   <div class="skill"><b>core</b>: HTML, CSS, Node.js and PHP<br /></div>
